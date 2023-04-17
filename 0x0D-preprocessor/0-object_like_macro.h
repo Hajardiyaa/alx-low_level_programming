e44900587a78100af6ce05e30ad6cb15a4a985b0
Hajardiyaa <hajardiyae12@gmail.com>
2023-04-17 15:43:55 -0700

    Preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef OBJECT_LIKE_MACRO_H   // Header guard to avoid multiple inclusions
+#define OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024    // Define the macro SIZE as 1024
+
+#endif
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+#ifndef PI_H   // Header guard to avoid multiple inclusions
+#define PI_H
+
+#define PI 3.14159265359    // Define the macro PI as 3.14159265359
+
+#endif  // End of header guard
