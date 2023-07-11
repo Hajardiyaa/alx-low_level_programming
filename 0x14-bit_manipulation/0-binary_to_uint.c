c4ad18f3c208ec985c9e8f98d01433dbfff474a4
Hajardiyaa <hajardiyae12@gmail.com>
2023-07-11 04:26:57 -0700

    test

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+unsigned int binary_to_uint(const char *b) {
+    if (b == NULL)
+        return 0;
+
+    unsigned int result = 0;
+    int i = 0;
+
+    while (b[i] != '\0') {
+        if (b[i] != '0' && b[i] != '1')
+            return 0;
+
+        result = result * 2 + _atoi(&b[i]);
+        i++;
+    }
+
+    return result;
+}
